df2036251e7fb1feb01a23ab14465e29b133896c
zhengjunqi <zhengjunqi@bytedance.com>
2024-10-23 11:13:48 +0800

    commit message

---
+++ components/democ/meta.ts
@@ -0,0 +1,14 @@
+import { IMeta } from '@byted-apaas/client-sdk/component-editor';
+
+const meta: IMeta = {
+  props: [
+    {
+      name: 'key1',
+      type: 'String',
+      title: '字符串',
+      defaultValue: 'string',
+    },
+  ],
+};
+
+export default meta;
